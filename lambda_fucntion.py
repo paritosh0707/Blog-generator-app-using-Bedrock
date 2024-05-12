1df7d14c650b6e3ef87b503913ef48d2c3b659a7
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-05-13 00:21:22 +0530

    updated the comments

--- lambda_fucntion.py
+++ lambda_fucntion.py
@@ -4,6 +4,15 @@ import botocore.config
 from datetime import datetime
 
 def generate_blog(blog_topic:str)->str:
+    """
+    This function generates a blog post on the given topic using the Amazon Bedrock service.
+
+    Args:
+        blog_topic (str): The topic for which the blog post needs to be generated.
+
+    Returns:
+        str: The generated blog post content.
+    """
     prompt = f"""<s>Human: Write a 50 words blog on the topic {blog_topic}\n\
     Assistant:[/INST]"""
 
@@ -15,11 +24,14 @@ def generate_blog(blog_topic:str)->str:
     }
 
     try:
+        # Create a Bedrock client with appropriate configuration
         bedrock_client = boto3.client(
             "bedrock-runtime",
             region_name = "us-east-1",
             config = botocore.config.Config(read_timeout=300,retries={"max_attempts":3})
         )
+        
+        # Invoke the Bedrock model to generate the blog post
         response = bedrock_client.invoke_model( modelId= "meta.llama3-8b-instruct-v1:0",
             contentType= "application/json",
             accept= "application/json",
@@ -35,6 +47,14 @@ def generate_blog(blog_topic:str)->str:
 
 
 def push_content_to_s3(content,s3_key,s3_bucket_name)->None:
+    """
+    This function uploads the generated blog post content to an S3 bucket.
+
+    Args:
+        content (str): The blog post content to be uploaded.
+        s3_key (str): The key (file path) for the object in the S3 bucket.
+        s3_bucket_name (str): The name of the S3 bucket.
+    """
     try:
         s3_client = boto3.client('s3')
         s3_client.put_object(
@@ -47,6 +67,16 @@ def push_content_to_s3(content,s3_key,s3_bucket_name)->None:
         print(f"Error occured while saving the file to S3 Bucket : {e}")
 
 def lambda_handler(event, context):
+    """
+    This is the Lambda function handler that orchestrates the blog generation and upload process.
+
+    Args:
+        event (dict): The event data received by the Lambda function.
+        context (object): The context object provided by AWS Lambda.
+
+    Returns:
+        dict: A dictionary containing the response status code and body.
+    """
     event_body = json.loads(event['body'])
     blog_topic = event_body['blog_topic']
     generated_blog = generate_blog(blog_topic=blog_topic)
